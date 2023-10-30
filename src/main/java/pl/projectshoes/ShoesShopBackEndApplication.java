7319b029c8a395948739c9cf64b719056723e221
CzarnaWoda <38265586+CzarnaWoda@users.noreply.github.com>
2023-10-30 18:57:47 +0100

    Initial commit

---
+++ src/main/java/pl/projectshoes/ShoesShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package pl.projectshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesShopBackEndApplication.class, args);
+	}
+
+}
