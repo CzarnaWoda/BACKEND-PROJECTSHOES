92e3fdea983d391db9d0b4c7ac58712b873339c1
CzarnaWoda <38265586+CzarnaWoda@users.noreply.github.com>
2023-11-02 11:20:13 +0100

    Update user structures

--- src/main/java/pl/projectshoes/user/repository/UserRepository.java
+++ src/main/java/pl/projectshoes/user/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package pl.projectshoes.user;
+package pl.projectshoes.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/pl/projectshoes/user/service/UserService.java
+++ src/main/java/pl/projectshoes/user/service/UserService.java
@@ -1,20 +1,19 @@
 package pl.projectshoes.user.service;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import pl.projectshoes.user.UserRepository;
+import pl.projectshoes.user.repository.UserRepository;
 import pl.projectshoes.user.model.User;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     public boolean isUserExist(String email){
         return userRepository.existsByEmail(email);
