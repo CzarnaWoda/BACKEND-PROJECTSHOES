a3b66d326b76fc9a9901662830f1e98257e29bbf
bartek-sudo <krolbartek14@gmail.com>
2023-11-02 12:30:08 +0100

    Update ProductRepository.java

--- src/main/java/pl/projectshoes/product/repository/ProductRepository.java
+++ src/main/java/pl/projectshoes/product/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package pl.projectshoes.product.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.projectshoes.product.model.Product;
 
